d48b435cadf4376503dce94b92916aa7dd708e7f
wiby <wibybramantyo123@gmail.com>
2020-04-11 17:38:11 +0700

    hari kamis

---
+++ exercise10.js
@@ -0,0 +1,35 @@
+function changeMe(arr) {
+    // you can only write your code here!
+    var obj = {}
+
+    for(i = 0; i < arr.length; i++){
+        for(j = 0; j < arr[i].length; j++){
+            if(j == 0){
+            console.log(i+1+'. '+arr[i][j]+' '+arr[i][j+1]+':');
+            }
+                obj.firstName = arr[i][0]
+                obj.lasstName = arr[i][1]
+                obj.gender = arr[i][2]
+           if(arr[i][3] != undefined){
+               obj.age = 2019 - arr[i][3]
+           }else{
+               obj.age = 'Invalid Birth Year'
+           }
+        }
+        console.log(obj)
+        // console.log(i,j)
+    }
+  }
+  
+  // TEST CASES
+  changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); // 1. Christ Evans:
+  // { firstName: 'Christ',
+  //   lastName: 'Evans',
+  //   gender: 'Male',
+  //   age: 37 }
+  // 2. Robert Downey:
+  // { firstName: 'Robert',
+  //   lastName: 'Downey',
+  //   gender: 'Male',
+  //   age: 'Invalid Birth Year' }
+  changeMe([]); // ""
---
+++ exercise11.js
@@ -0,0 +1,63 @@
+function shoppingTime(memberId, money) {
+    // you can only write your code here!
+    var tokox =
+    {
+        "Sepatu Stacattu": 1500000,
+        "Baju Zoro": 500000,
+        "Baju H&N": 250000,
+        "Sweater Uniklooh": 175000,
+        "Casing Handphone": 50000
+    }
+
+    var buyer = 
+    {
+        memberId:memberId,
+        money:money,
+        listPurchased:[],
+        changeMoney:0
+    }
+
+    var listbarang=[];
+  
+  if (memberId == ''){
+    return "Mohon maaf, toko X hanya berlaku untuk member saja";
+  }else if (memberId == undefined){
+    return "Mohon maaf, toko X hanya berlaku untuk member saja"
+  }
+  else if(money<50000){
+    return "Mohon maaf, uang tidak cukup";
+  }else if(memberId !=='' && money>50000){
+    for(var key in tokox){
+      if(money>=tokox[key]){
+        listbarang.push(key);
+        money=money-tokox[key];
+
+      } 
+    }
+  }
+  buyer.listPurchased=listbarang;
+  buyer.changeMoney=money;
+  return buyer
+  
+  }
+  
+  // TEST CASES
+  console.log(shoppingTime('1820RzKrnWn08', 2475000));
+    //{ memberId: '1820RzKrnWn08',
+    // money: 2475000,
+    // listPurchased:
+    //  [ 'Sepatu Stacattu',
+    //    'Baju Zoro',
+    //    'Baju H&N',
+    //    'Sweater Uniklooh',
+    //    'Casing Handphone' ],
+    // changeMoney: 0 }
+  console.log(shoppingTime('82Ku8Ma742', 170000));
+  //{ memberId: '82Ku8Ma742',
+  // money: 170000,
+  // listPurchased:
+  //  [ 'Casing Handphone' ],
+  // changeMoney: 120000 }
+  console.log(shoppingTime('', 2475000)); //Mohon maaf, toko X hanya berlaku untuk member saja
+  console.log(shoppingTime('234JdhweRxa53', 15000)); //Mohon maaf, uang tidak cukup
+  console.log(shoppingTime()); ////Mohon maaf, toko X hanya berlaku untuk member saja
